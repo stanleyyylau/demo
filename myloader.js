e793d0a24edbc733e332a7e7f30240709a3f173e
Stanley Lau <stanley@mmldigi.com>
2018-09-06 17:19:52 +0800

    rewriting logger

--- myloader.js
+++ myloader.js
@@ -1,7 +1,7 @@
 // Identity loader
 module.exports = function(source) {
-    if(this.context.indexOf('epc-logger') > 0) {
-        console.log('############ epc logger ###### ')
+    if(this.context.indexOf('client-logger') > 0) {
+        console.log('############ writing logger ###### ')
         return JSON.stringify("export const timeToInteraction = { registerEvent: (name)=>{console.log('regisger event')}, unRegisterEvent: (name)=>{console.log('unresig event')} };")
     }
     return source;
